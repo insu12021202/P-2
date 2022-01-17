cc504796e44821289d795e06832134401aa0bf12
insu12021202 <insu12021202@gmail.com>
2022-01-17 23:19:16 +0900

    메인 페이지, 로그인 페이지, 회원가입 페이지, 상세 페이지 레이아웃 작성(HTML,CSS)/ 상세 페이지 캐러셀 기능 구현

---
+++ frontend/JS/carousel.js
@@ -0,0 +1,23 @@
+const preBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const carousel = document.querySelector('.carousel');
+
+let index = 0; // 사진의 개수만큼
+
+preBtn.addEventListener('click', () => {
+    if(index === 0){
+        return;
+    }
+    index -= 1;
+
+    carousel.style.transform = `translate3d(-${500 * index}px, 0, 0)`;
+});
+
+nextBtn.addEventListener('click', () => {
+    if(index === 4){ //사진의 개수 -1
+        return;
+    }
+    index += 1;
+
+    carousel.style.transform = `translate3d(-${500 * index}px, 0, 0)`;
+});
