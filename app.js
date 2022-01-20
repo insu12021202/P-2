f4d98caef28548bdaccb1aba733025335a76ff10
Dongjin Kuk <puter2712goo@gmail.com>
2022-01-21 00:28:11 +0900

    Trying to Create Login/Sign Up System using Passport.js

--- app.js
+++ app.js
@@ -1,56 +1,110 @@
-const express = require('express');
-const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const crypto = require('crypto');
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+/* session */
+app.use(session({
+	key: 'session_cookie_name',
+	secret: 'session_cookie_secret',
+	store: new MySQLStore({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '111111',
+		database: 'cookie_project2'
+	}),
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24,
+	}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 /* MySQL 연결 */
-const mysql = require('mysql2');
 const connection = mysql.createConnection({
-	host: 'localhosconstt',
+	host: 'localhost',
 	user: 'root',
 	password: '111111',
 	database: 'project2',
 	multipleStatements: true
 });
-// connection.connect((err) => {
-// 	if (!err) {
-// 		console.log("Connected");
-// 	} else {
-// 		console.log("Connection Failed");
-// 	}
-// });
+connection.connect((err) => {
+	if (!err) console.log("Connected");
+	else console.log("Connection Failed");
+});
+
+const customFields = {
+	usernameField: 'uname',
+	passwordField: 'pw',
+};
+
+const verifyCallback = (username, password, done) => {
+	connection.query('SELECT * FROM login WHERE username = ?', [username], function(error, results, fields) {
+		if (error) return done(error);
+		if (results.length == 0) return done(null, false);
+		const isValid = validPassword(password, results[0].hash, results[0].salt);
+		login = { login_id: results[0].login_id, username: results[0].username, hash: results[0].hash, salt: results[0].salt };
+		if (isValid) return done(null, user);
+		else return done(null, false);
+	});
+}
+
+const strategy = new LocalStrategy(customFields, verifyCallback);
+passport.use(strategy);
+
+passport.serializeUser((user, done) => done(null, user.login_id));
+
+passport.deserializeUser(function(userId, done) {
+	connection.query('SELECT * FROM login WHERE login_id = ?', [userId], function(error, results) {
+		done(null, results[0]);
+	});
+});
+
+function validPassword(password, hash, salt) {
+	const hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 60, 'sha512').toString('hex');
+	return hash === hashVerify;
+}
+
+function genPassword(password) {
+	const salt = crypto.randomBytes(32).toString('hex');
+	const genhash = crypto.pbkdf2Sync(password, salt, 10000, 60, 'sha512').toString('hex');
+	return {salt: salt, hash: genhash};
+}
+
+function isAuth(req, res, next) {
+	if (req,isAuthenticated()) next();
+	else res.redirect('/notAuthorized');
+}
+
+function isAdmin(req, res, next) {
+	if (req.isAuthenticated() && req.user.isAdmin == 1) next();
+	else res.redirect('/notAuthorizedAdmin');
+}
+
+function userExists(req, res, next) {
+	connection.query('SELECT * FROM login WHERE username = ?', [req.body.uname], function (error, results, fields) {
+		if (error) console.log("Error");
+		else if (results.length > 0) res.redirect('/userAlreadyExists');
+		else next();
+	});
+}
 
 app.set('port', process.env.PORT || 3000);	// 포트 (3000)
-app.use(express.static('public'));
-
-// app.use(session({
-// 	key: 'session_cookie_name',
-// 	secret: 'session_cookie_secret',
-// 	store: new MySQLStore({
-// 		host: 'localhost',
-// 		port: 3306,
-// 		user: 'root',
-// 		database: 'project2'
-// 	}),
-// 	resave: false,
-// 	saveUninitialized: false,
-// 	cookie: {
-// 		maxAge: 1000 * 60 * 60 * 24,
-// 	}
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-// 	extended: true
-// }));
-// app.use(express.static('public'));
-// app.use('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
