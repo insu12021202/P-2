6e51d15382cea8e9d16aaa6ff784887d683cd1f7
jin-Pro <70205497+jin-Pro@users.noreply.github.com>
2022-01-23 13:48:57 +0900

    Revert "회원가입 기능 구현"

--- public/deleteNodes.js
+++
@@ -1,6 +0,0 @@
-export function deleteNodes(parentClass) {
-	const parentNode = document.querySelector(parentClass);
-	while (parentNode.hasChildNodes()) {
-		parentNode.removeChild(parentNode.firstChild);
-	}
-}
--- public/header/header.js
+++
@@ -1,6 +0,0 @@
-import {makeElement} from '../makeHTMLElement.js';
-import { makeHeaderDiv } from './headerTopDiv.js';
-
-/* Header 태그 */
-export const header = makeElement('header', null, null);
-makeHeaderDiv(header);
